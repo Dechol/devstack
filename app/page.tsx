c10b48ae3a0f318feba14b2188817c22983a0725
declan <declanholland91@gmail.com>
2023-10-01 03:58:42 +0700

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">next 13 header</h1>
-      <h1 className="h2-bold">next 13 header</h1>
-      <h1 className="h3-bold">next 13 header</h1>
+      <h2 className="h2-bold">next 13 header</h2>
+      <h3 className="h3-bold">next 13 header</h3>
     </div>
   );
 }
